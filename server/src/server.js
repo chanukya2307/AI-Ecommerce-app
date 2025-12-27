2dee1fa48ab70f21d51513c1422cf889ee1eca6d
Chanukya <chanukyachintha2307@gmail.com>
2025-12-27 18:59:20 +0530

    final commit

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 await connectDB(); 
 
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
+
+export default app;
