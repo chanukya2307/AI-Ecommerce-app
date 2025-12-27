77c64f7c8fa58ee7bbe01a6a12169656d04e6a0c
Chintha Chanukya Kumar <159230191+chanukya2307@users.noreply.github.com>
2025-12-27 10:26:21 +0530

    Change API base URL to remove '/api' suffix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://shopssy-backend.onrender.com/api",
+  baseURL: "https://shopssy-backend.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
